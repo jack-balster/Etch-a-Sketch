1f90d08870944e2f7683a5358fdc7a43d8bf7e5c
Jack Balster <107078414+jack-balster@users.noreply.github.com>
2022-08-08 19:55:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const canvasContainer = document.querySelector('.canvas-container');
-const slider = document.querySelector('#grid-slider');
+const canvasContainer = document.querySelector('.canvas-container'); //. = css element
+const slider = document.querySelector('#grid-slider'); //# = id in html
 const gridSizeLabel = document.querySelector('.grid-size-label');
 const shakeButton = document.querySelector('#shake-button');
 const appContainer = document.querySelector('.app-container');
@@ -7,30 +7,33 @@ const colorSelector = document.querySelector('#color-selector');
 const modeSelector = document.querySelector('#mode-selector');
 const colorControl = document.querySelector('#color-control');
 
-//grid ratio - wanted to use a wide format like real etch-a-sketch
+//grid ratio - wanted to use a wide format like real etch-a-sketch (rectangle)
 const gridRatio = 2; 
 
+//default left-click is false
 let mouseDown = false
-window.onmousedown = () => mouseDown = true;
-window.onmouseup = () => mouseDown = false;
+window.onmousedown = () => mouseDown = true; //left-click mouse down = true
+window.onmouseup = () => mouseDown = false; //stop left-click mouse down = false
 
 //default color
 let colorChoice = "Classic";
 colorSelector.textContent = colorChoice;
-colorSelector.addEventListener('click',setColor);
+colorSelector.addEventListener('click',setColor); //on click set color
 
-const colorPicker = document.createElement('input');
-colorPicker.setAttribute('type','color','class','picker');
-colorPicker.addEventListener('input',pickColor);
+//creates input to get color 
+const colorPicker = document.createElement('input'); //creates input element in html for color
+colorPicker.setAttribute('type','color','class','picker'); 
+colorPicker.addEventListener('input',pickColor); //picks color after input
 
 //default color picker is black
 let pickedColor = "#000"
 
-function pickColor(e){
+function pickColor(e){ //e = event parameter
   console.log(e.target.value);
-  pickedColor = e.target.value;
+  pickedColor = e.target.value; //value of target property in DOM (value of something on webpage)
 }
 
+//when user clicks color it should go to the next in the list
 function setColor () {
   if (colorChoice === "Classic") {
   colorChoice = "Rainbow";
@@ -52,6 +55,7 @@ let drawMode = "Draw";
 modeSelector.textContent = drawMode;
 modeSelector.addEventListener('click',setMode);
 
+//sets to draw or erase
 function setMode () {
   if (drawMode === "Draw") {
     drawMode = "Erase";
@@ -61,17 +65,17 @@ function setMode () {
   modeSelector.textContent = drawMode;
 };
 
-let currentSize = 12;
+let currentSize = 12; //default size will be 12 x 24
 //input default canvas settings
 defaultCanvas(currentSize,5,50); //starting rows, min rows, max rows
 
 //setup default canvas
 function defaultCanvas(size,min,max) {
   changeGridSize(size);
-  slider.setAttribute('value',size);
+  slider.setAttribute('value',size); //sets value of value
   slider.setAttribute('min',min);
   slider.setAttribute('max',max);
-  gridSizeLabel.textContent = `Grid size: ${size} x ${size * gridRatio}`; 
+  gridSizeLabel.textContent = `Grid size: ${size} x ${size * gridRatio}`; //displays size * 2 (grid ratio)
 };
 
 //update the current slider value (each time you drag the slider handle)
@@ -81,6 +85,7 @@ slider.oninput = function sliderInput() {
   changeGridSize(this.value);
 };
 
+//sets new grid size
 function changeGridSize(gridSize) {
   setupNewCanvas(gridSize);
   addSquares(gridSize);
@@ -89,43 +94,46 @@ function changeGridSize(gridSize) {
 //clear the canvas and then add CSS grid properties to the canvas element
 function setupNewCanvas(gridSize) {
   canvasContainer.innerHTML = '';
+  //1 fraction of column grid size so 20 would = 1/20
   canvasContainer.style.gridTemplateColumns = `repeat(${gridSize * gridRatio}, 1fr)`;
+  //1 fraction of row grid size so 20 would = 1/20
   canvasContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 };
 
 //add the correct amount of divs to the grid
 function addSquares(gridSize) {
   for (let i = 1; i <= gridSize * (gridSize * gridRatio); i++) {
-  const gridSquare = document.createElement('div');
+  const gridSquare = document.createElement('div');//create new div for every square
 
   gridSquare.classList.add('grid-square');
-  gridSquare.id = `Sq${i}`;
-  gridSquare.addEventListener('mousedown', setBg);
-  gridSquare.addEventListener('mouseover', mousetrail);
-  canvasContainer.appendChild(gridSquare);
+  gridSquare.id = `Sq${i}`; //sets new id for grid square
+  gridSquare.addEventListener('mousedown', setBg); //clicking on square
+  gridSquare.addEventListener('mouseover', mousetrail); //hover on square
+  canvasContainer.appendChild(gridSquare); //adds updated grid squares to canvas container element
   };  
 };
 
-shakeButton.addEventListener('click',shakeCanvas);
+shakeButton.addEventListener('click',shakeCanvas); //when shake is clicked execute function
 
-//adds a class containing animation for the shake
+//animation for the shake
 function shakeCanvas() {
-  const squares = document.querySelectorAll('.grid-square');
-  squares.forEach(square => square.style.backgroundColor = "");
+  const squares = document.querySelectorAll('.grid-square'); //selects all squares
+  squares.forEach(square => square.style.backgroundColor = ""); //clears each square
   appContainer.classList.add('canvas-shake');
   //when the animation is finished, remove the class so it's ready to run again
   appContainer.addEventListener('animationend',() => appContainer.classList.remove('canvas-shake'));
 };
 
+//function to show mouse hover/trail
 function mousetrail(e) {
- if (mouseDown === true) {
+ if (mouseDown === true) { //if mouse clicked then set color
   setBg(e);
- }
+ } //else add hover class to square and remove when not on square
  e.target.classList.add('hover');
  e.target.addEventListener('transitionend',() => e.target.classList.remove('hover'));
 };
 
-//change the background color of the squares
+//function to change the background color of the squares
 function setBg(e){
   if (drawMode === "Draw") {
     if(colorChoice === "Classic") {
@@ -133,7 +141,7 @@ function setBg(e){
     } else if (colorChoice === "Rainbow"){
         rainbow(e);
     } else if (colorChoice === "Grey/Tint"){
-      tintBg(e);
+      tintBg(e); //tint background
     } else if (colorChoice === "Retro"){
       colorSwatches(e);
     } else if (colorChoice === "Picker"){
@@ -145,13 +153,13 @@ function setBg(e){
   };
 };
 
-//this variable references the array index of the color we want to use
+//this variable references the array index of the color to use
 let colorSelection = 0;
 
 //rainbow color
 function rainbow(e) {
     const colors = ["#ff2f00", "#00ff1e", "#ff00ee", "#00f2ff", "#ffee00", "#ff9900"];
-    if(colorSelection > colors.length - 1){colorSelection = 0}; 
+    if(colorSelection > colors.length - 1){colorSelection = 0}; //goes back to first color (loops of 6)
     e.target.style.backgroundColor = colors[colorSelection];
     colorSelection++;
   }
@@ -159,14 +167,14 @@ function rainbow(e) {
 //retro color
 function colorSwatches(e) {
   const colors = ["#3F8A8C", "#0C5679", "#0B0835", "#E5340B", "#F28A0F", "#FFE7BD"];
-  if(colorSelection > colors.length - 1){colorSelection = 0}; 
+  if(colorSelection > colors.length - 1){colorSelection = 0}; //goes back to first color (loops of 6)
   e.target.style.backgroundColor = colors[colorSelection];
   colorSelection++;
 }
 
-//grey/tint color
+//grey/tint color (from stack overflow)
 function tintBg(e) {
-  //if there's no rgb color on the square, set it to the lightest tint
+  //if there's no rgb color on the square, set it to the lightest tint of grey
   if(e.target.style.backgroundColor === "") {
     e.target.style.backgroundColor = "rgb(180, 180, 180)";
 
@@ -184,7 +192,7 @@ function tintBg(e) {
       const newValue = currentArray[0] - 40;
       e.target.style.backgroundColor = `rgb(${newValue}, ${newValue}, ${newValue})`
 
-    //if it's a color other than grey, also set it to the lightest tint
+    //if it's a color other than grey, set it to the lightest tint of grey
     } else {
       e.target.style.backgroundColor = "rgb(180, 180, 180)";
     };
